102f8011a8974be6715592c27624f6a7d69a0c07
StevenWang88 <705067399@qq.com>
2018-05-31 15:23:28 +0800

    second code

--- source/utils/lazyload.js
+++ source/utils/lazyload.js
@@ -1,8 +1,20 @@
-import aContainer from 'bundle-loader?lazy!./containers/A'
+// import aContainer from 'bundle-loader?lazy!./containers/A'
 //这里只是给this.props.child传一个方法，最后在Bundle的render里面调用
+import React from 'react'
+import Bundle from './bundle'
 
-const A = (props) => (
-  <Bundle load={aContainer}>
-    {(Container) => <Container {...props}/>}
-  </Bundle>
-)
+// const A = (props) => (
+//   <Bundle load={aContainer}>
+//     {(Container) => <Container {...props}/>}
+//   </Bundle>
+// )
+
+export default (component) => (props) => {
+  return (
+    <Bundle load={component}>
+      {
+        (Component) => Component ? <Component {...props} /> : false
+      }
+    </Bundle>
+  )
+}
